1836f4ab0e1c77c083d02ceb319bc7ef23da9b8a
Nikita Antonov <nikita45454@gmail.com>
2023-09-14 21:42:04 +0300

    адаптирует куки модалку

--- src/core/AppButton.vue
+++ src/core/AppButton.vue
@@ -1,7 +1,7 @@
 <template>
   <component
     :is="tag"
-    class="tw-rounded-[14px] tw-text-white tw-bg-yellow tw-text-center tw-py-20 tw-px-20 lg:tw-py-15 lg:tw-px-30 hover:tw-bg-orangeHover active:tw-bg-opacity-90 disabled:tw-bg-dark disabled:tw-text-secondary"
+    class="tw-rounded-[16px] tw-text-white tw-bg-orange tw-text-center tw-py-20 tw-px-20 lg:tw-py-15 lg:tw-px-30 hover:tw-bg-orangeHover active:tw-bg-opacity-90 disabled:tw-bg-dark disabled:tw-text-secondary"
     :class="{ disable: disabled }">
     <slot />
   </component>
