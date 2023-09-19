403a731636738fd88de2d6a1d2ca55ec2e85245a
Nikita Antonov <nikita45454@gmail.com>
2023-09-19 23:15:06 +0300

    адаптирует 404

--- src/pages/Fallback404.vue
+++ src/pages/Fallback404.vue
@@ -1,15 +1,15 @@
 <template>
   <app-page class="2xl:tw-pt-[100px] page-pb">
     <div
-      class="wrapper tw-relative decor-rock after:tw-content-none 2xl:after:tw-content-['']">
+      class="wrapper tw-relative after:tw-content-none 2xl:after:tw-content-['']">
       <div class="xl:tw-flex xl:tw-justify-between">
         <div class="">
           <p
-            class="tw-font-stolz -tw-tracking-sm tw-text-lg tw-leading-120 md:tw-text-xl 2xl:tw-text-xl10 tw-mb-20 md:tw-mb-40 xl:tw-mb-20">
+            class="tw-font-nord tw-uppercase tw-text-white -tw-tracking-sm tw-text-lg tw-leading-120 md:tw-text-xl 2xl:tw-text-xl10 tw-mb-20 md:tw-mb-40 xl:tw-mb-20">
             Упс ...
           </p>
           <h1
-            class="tw-text-gray tw-text-base tw-leading-120 tw-max-w-[260px] tw-mb-60 md:tw-max-w-[600px] xl:tw-text-md md:tw-font-extrabold md:tw-mb-60">
+            class="tw-text-white tw-text-base tw-leading-120 tw-max-w-[260px] tw-mb-60 md:tw-max-w-[600px] xl:tw-text-md md:tw-mb-60">
             Извините, страница не найдена
           </h1>
         </div>
@@ -24,20 +24,15 @@
                   height="107"
                   src="@/assets/images/fallback.svg"
                   alt="404" />
-
-                <span
-                  class="tw-font-misto tw-text-red tw-text-lg 2xl:tw-text-xl tw-leading-120 tw-absolute tw-top-1/2 tw-left-1/2 tw-transform -tw-translate-x-1/2 -tw-translate-y-1/2 -tw-rotate-[15deg]">
-                  Ошибка
-                </span>
               </div>
             </div>
             <div class="">
-              <AppLink
-                class="md:tw-inline-flex !tw-text-gray"
-                external
-                :to="{ name: 'main' }"
-                >На главную</AppLink
+              <AppButton
+                class="tw-w-full tw-max-w-sm"
+                @click="$router.replace({ name: 'main' })"
               >
+                На главную
+              </AppButton>
             </div>
           </div>
         </div>
