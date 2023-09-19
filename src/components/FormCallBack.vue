a81d09953c9dcba35deacf62c66011a201276413
Nikita Antonov <nikita45454@gmail.com>
2023-09-19 11:56:11 +0300

    адаптирует форму обратной связи

--- src/components/FormCallBack.vue
+++ src/components/FormCallBack.vue
@@ -1,48 +1,91 @@
 <template>
-  <div class="tw-bg-dark tw-rounded-[24px] tw-py-[45px] tw-px-16 md:tw-px-[36px] 2xl:tw-py-[76px] 2xl:tw-px-[145px] tw-relative">
-    <div class="xl:tw-flex 2xl:tw-block">
-      <div class="xl:tw-basis-[420px] xl:tw-mr-40 2xl:tw-mr-0">
-        <p class="tw-text-lg tw-text-white tw-font-stolz tw-mb-10 2xl:tw-text-xl">
-          Заказать звонок
-        </p>
-        <p class="tw-text-md tw-leading-120 tw-text-gray tw-mb-30 tw-max-w-[320px] md:tw-max-w-[430px]">
-          Оставьте заявку и&nbsp;наши менеджеры свяжутся с&nbsp;вами в&nbsp;ближайшее время
-        </p>
-      </div>
-      <Form class="tw-grow" v-slot="{ isSubmitting }" @submit="submit">
-        <div class="md:tw-flex md:tw-space-x-20 md:tw-items-center">
+  <div class="tw-p-16 tw-pb-32 md:tw-p-40 tw-bg-base-700 tw-flex tw-flex-wrap lg:tw-h-[378px] 2xl:tw-h-[710px]">
+    <div v-if="!pageView" class="tw-basis-full lg:tw-grow-[2] tw-h-[270px] lg:tw-h-full lg:tw-basis-[400px] lg:tw-pr-20 tw-mb-32 lg:tw-mb-0">
+      <yandex-map
+        v-if="coords && address"
+        class="tw-mix-blend-luminosity tw-relative tw-z-10 tw-h-full tw-w-full"
+        :coords="coords"
+        :zoom="17"
+        :controls="[]">
+        <ymap-marker
+          marker-id="place"
+          :coords="coords"
+          :hint-content="`офис - ${address}`" />
+      </yandex-map>
+    </div>
+    <div
+      class="tw-h-full tw-flex tw-flex-col tw-justify-between tw-basis-full lg:tw-basis-[200px] lg:tw-pr-20"
+      :class="{
+        'lg:tw-grow': !pageView,
+        'lg:tw-grow-[2]': pageView
+       }"
+    >
+      <p class="tw-text-[32px] tw-leading-120 tw-uppercase tw-font-nord tw-text-white 2xl:tw-text-xl tw-max-w-[254px]">
+        Заказать звонок
+      </p>
+      <p class="tw-text-third tw-text-base">
+        Оставьте заявку и&nbsp;наши менеджеры
+        <br>
+        свяжутся с&nbsp;вами в&nbsp;ближайшее время
+      </p>
+    </div>
+    <div class="tw-h-full  tw-basis-full lg:tw-grow lg:tw-basis-[200px]">
+      <Form class="tw-h-full tw-flex tw-flex-col tw-justify-between" v-slot="{ isSubmitting }" @submit="submit">
+        <div class="">
           <AppInput
-            class="md:tw-basis-[190px]"
+            class="tw-w-full"
             rules="required"
             name="name"
             label="Ваше имя"
           />
           <AppInput
-            class="md:tw-basis-[190px]"
+            class="tw-w-full"
             rules="required|cellphone"
             name="cellphone"
             label="Номер телефона"
             type="tel"
           />
+        </div>
+        <div>
           <AppButton
-            class="tw-mt-20 tw-w-full tw-py-15 md:tw-mt-0 md:tw-basis-[200px]"
+            class="tw-w-full tw-mb-24"
             type="submit"
             :disabled="isSubmitting"
           >
             Отправить
           </AppButton>
+          <AppCheckbox class="" name="agreement" label="Условия" rules="required" :value="true">
+            Я согласен с <AppLink native to="/policy.pdf" target="_blank">условиями передачи информации</AppLink>
+          </AppCheckbox>
         </div>
-        <AppCheckbox class="tw-mt-30 tw-text-gray tw-text-xs md:tw-mt-10 xl:tw-mt-25" name="agreement" label="Условия" rules="required" :value="true">
-          Я согласен с <AppLink native to="/policy.pdf" target="_blank">условиями передачи информации</AppLink>
-        </AppCheckbox>
       </Form>
     </div>
-    <slot />
   </div>
 </template>
 <script>
+import { mapActions, mapGetters } from "vuex";
+
 export default {
+  props: {
+    pageView: {
+      default: false,
+      type: Boolean
+    }
+  },
+  async created() {
+    if (this.address === null) {
+      await this.getAddress();
+    }
+
+    if (this.coords === null) {
+      await this.getCoords();
+    }
+  },
+  computed: {
+    ...mapGetters(["address", "coords", "worktime"]),
+  },
   methods: {
+    ...mapActions(["getAddress", "getCoords", "getWorktime"]),
     async submit({ name, cellphone }) {
       await this.$store.dispatch('getFeedback', { name, cellphone, theme: 'Обратный звонок' });
       this.$notify({ type: 'success', text: 'Ваша заявка успешно отправлена!' });
